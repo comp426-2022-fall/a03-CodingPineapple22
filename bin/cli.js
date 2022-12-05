d23cdb10c40f018e33adc7cb787561ed4234a012
CodingPineapple22 <leothomasthefirst@gmail.com>
2022-12-05 01:51:07 -0500

    did chmod +x ./bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,15 @@
 #!/usr/bim/env node
 import minimist from 'minimist';
-import { roll } from "./lib/roll.js"
+import { roll } from "/lib/roll.js"
 
 const arg = minimist(process.argv.slice(2));
 
 var sides, dice, rolls = 0; 
 
-if(arg.sides != null){ sides = arg.sides;}else{sides = 6;}
+if(arg.sides){ sides = arg.sides;}else{sides = 6;}
 
-if(arg.dice != null){ dice = arg.dice;}else{dice = 2;}
+if(arg.dice){ dice = arg.dice;}else{dice = 2;}
 
-if(arg.rolls != null){rolls = arg.rolls;}else{rolls = 1;}
+if(arg.rolls){rolls = arg.rolls;}else{rolls = 1;}
 
-console.log(JSON.stringify(roll(sides, dice, rolls)));
+console.log(roll(sides, dice, rolls)); Z
