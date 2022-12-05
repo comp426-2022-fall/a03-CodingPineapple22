14b6702c816e950a5b8ff975ac614d73d1919c3d
CodingPineapple22 <leothomasthefirst@gmail.com>
2022-12-05 02:33:29 -0500

    k

--- bin/cli.js
+++ bin/cli.js
@@ -4,12 +4,9 @@ import { roll } from "/lib/roll.js"
 
 const arg = minimist(process.argv.slice(2));
 
-var sides, dice, rolls = 0; 
-
-
-sides = arg.side ? arg.sides : 6
-dice = arg.dice ? arg.dice:2;
-rolls = arg.rolls? arg.rolls: 1; 
+const sides = arg.side ? arg.sides : 6
+const dice = arg.dice ? arg.dice:2;
+const rolls = arg.rolls? arg.rolls: 1; 
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
 
