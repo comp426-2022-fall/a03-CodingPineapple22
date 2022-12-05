1f6ba443d0ef58731329e0f8a625f9a24974a34f
CodingPineapple22 <leothomasthefirst@gmail.com>
2022-12-05 01:25:32 -0500

    small ups2

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,14 @@
 #!/usr/bim/env node
 import minimist from 'minimist';
-import { roll } from "/lib/roll.js"
+import { roll } from "../lib/roll.js"
 
 const arg = minimist(process.argv.slice(2));
 
 var sides, dice, rolls = 0; 
 
-if(arg.sides != null && arg.sides > 0){ sides = arg.sides;}else{sides = 6;}
-
-if(arg.dice != null && arg.dice>0){ dice = arg.dice;}else{dice = 2;}
-
-if(arg.rolls != null && arg.rolls>0){rolls = arg.rolls;}else{rolls = 1;}
+sides = arg.side ? arg.sides : 6
+dice = arg.dice ? arg.dice:2;
+rolls = arg.rolls? arg.rolls: 1; 
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
+
