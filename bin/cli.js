08898a14662ca7d445db119d4959386bfac54b7e
CodingPineapple22 <leothomasthefirst@gmail.com>
2022-12-05 01:17:55 -0500

    minor edits1

--- bin/cli.js
+++ bin/cli.js
@@ -6,10 +6,10 @@ const arg = minimist(process.argv.slice(2));
 
 var sides, dice, rolls = 0; 
 
-if(arg.sides != null){ sides = arg.sides;}else{sides = 6;}
+if(arg.sides != null && arg.sides > 0){ sides = arg.sides;}else{sides = 6;}
 
-if(arg.dice != null){ dice = arg.dice;}else{dice = 2;}
+if(arg.dice != null && arg.dice>0){ dice = arg.dice;}else{dice = 2;}
 
-if(arg.rolls != null){rolls = arg.rolls;}else{rolls = 1;}
+if(arg.rolls != null && arg.rolls>0){rolls = arg.rolls;}else{rolls = 1;}
 
-console.log(roll(sides, dice, rolls));
+console.log(JSON.stringify(roll(sides, dice, rolls)));
