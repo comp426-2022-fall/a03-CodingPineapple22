1b6113d730413600ebf5ea931c5ddaa2d53d97b4
Leo Kalapurackal <54859310+CodingPineapple22@users.noreply.github.com>
2022-12-07 13:31:15 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,13 @@
 #!/usr/bim/env node
+import { roll } from "../lib/roll.js"
 import minimist from 'minimist';
-import { roll } from "/lib/roll.js"
+
 
 const arg = minimist(process.argv.slice(2));
 
 const sides = arg.sides ? arg.sides : 6
-const dice = arg.dice ? arg.dice:2;
-const rolls = arg.rolls? arg.rolls: 1; 
+const dice = arg.dice ? arg.dice : 2;
+const rolls = arg.rolls ? arg.rolls: 1; 
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
 
